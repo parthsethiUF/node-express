94acdfafe67d8ac2d9cb45cfe9958722fbd91bdb
parth <parthsethi16.ps@gmail.com>
2018-08-13 19:13:41 -0400

    router_services

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');// to parse the body of request ex. ma
 
 
 const dishRouter = require('./routes/dishRouter');
-
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 
 const hostname = 'localhost';
@@ -17,6 +18,8 @@ app.use(bodyParser.json());
 
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 /*
 app.all('/dishes', (req, res, next) => {
